c5b1faf4a8b5cacf1d3128b8330022b438abb6cf
Andej Nesterenko <mentatus.n@gmail.com>
2022-02-16 20:16:40 +0300

    урок 2 закончили

--- app/src/main/java/com/gb/material_1728_3/view/MainActivity.kt
+++ app/src/main/java/com/gb/material_1728_3/view/MainActivity.kt
@@ -2,15 +2,24 @@ package com.gb.material_1728_3.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.TextView
 import com.gb.material_1728_3.R
 import com.gb.material_1728_3.view.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(R.style.MyThemeBlue)
+        // SP
+        setTheme(R.style.MyBlackBlue)
         setContentView(R.layout.activity_main)
 
+        val textView = TextView(this).apply {
+            text=""
+            textSize=30f
+            gravity = Gravity.NO_GRAVITY
+
+        }
 
         if(savedInstanceState==null){
             supportFragmentManager.beginTransaction().replace(R.id.container,MainFragment.newInstance()).commit()
