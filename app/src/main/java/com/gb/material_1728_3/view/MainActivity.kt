8f263b45b8ad32a01aeca7fb2699e4055b52d525
Andej Nesterenko <mentatus.n@gmail.com>
2022-02-25 20:37:21 +0300

    урок 4 ConstraintLayout: barrier,guideLine,group

--- app/src/main/java/com/gb/material_1728_3/view/MainActivity.kt
+++ app/src/main/java/com/gb/material_1728_3/view/MainActivity.kt
@@ -3,16 +3,20 @@ package com.gb.material_1728_3.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import androidx.constraintlayout.widget.Group
 import com.gb.material_1728_3.R
 import com.gb.material_1728_3.view.main.MainFragment
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // SP
         setTheme(R.style.MyBlackBlue)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_test_layout)
 
         val textView = TextView(this).apply {
             text=""
@@ -22,8 +26,13 @@ class MainActivity : AppCompatActivity() {
         }
 
         if(savedInstanceState==null){
-            supportFragmentManager.beginTransaction().replace(R.id.container,MainFragment.newInstance()).commit()
+            //supportFragmentManager.beginTransaction().replace(R.id.container,MainFragment.newInstance()).commit()
         }
+
+        /*findViewById<Button>(R.id.button1).visibility = View.GONE
+        findViewById<Button>(R.id.button2).visibility = View.GONE*/
+
+        //findViewById<Group>(R.id.group1).visibility = View.GONE
 
 //        recreate()
     }
